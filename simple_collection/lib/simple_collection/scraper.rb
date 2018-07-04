0b8e2233dbd96eaf313b8353daad1ebbeb478c1f
Heidi Engstrom <hengstrom25@gmail.com>
2018-07-04 01:03:06 +0000

    automatically backed up by learn

--- simple_collection/lib/simple_collection/scraper.rb
+++ simple_collection/lib/simple_collection/scraper.rb
@@ -4,7 +4,7 @@ class SimpleCollection::Scraper
     doc = Nokogiri::HTML(open("http://tincanknits.com/thesimplecollection.html"))
     page = doc.css("tr td a img.img-thumbnail-tight")
     patterns = page.map {|img|
-      Pattern.new(img.attr("alt"), img.parent.attr("href"))}
+      SimpleCollection::Pattern.new(img.attr("alt"), img.parent.attr("href"))}
     patterns = patterns.uniq {|patt| patt.name}
     patterns.select {|patt|
       patt.name != "Handmade in the UK" && patt.name != "Knitting Basics PDF" && patt.name != "Grain by Tin Can Knits"}
